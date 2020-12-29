85ad23bfb3e34dda48d8da5c35d41ccfc2186d6e
Cheok Yan Cheng <yancheng.cheok@gmail.com>
2020-12-30 02:14:19 +0800

    Comment

--- MyApplication/app/src/main/java/com/yocto/myapplication/Migration_1_2.java
+++ MyApplication/app/src/main/java/com/yocto/myapplication/Migration_1_2.java
@@ -11,6 +11,11 @@ public class Migration_1_2 extends Migration {
 
     @Override
     public void migrate(@NonNull SupportSQLiteDatabase database) {
-        // We forget to include index creation code for user's first_name
+        // We forget to include index creation code for user's first_name. The unit test suppose to
+        // fail. But it is not. Why?
+
+
+        // Uncomment the following line, to confirm Room unit test is checking for table schema.
+        //database.execSQL("DROP TABLE user");
     }
 }
